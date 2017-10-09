a497b37e49e9fffcbab9160af50f799c9ed7bc93
William Schoenell <wschoenell@gmail.com>
2017-10-09 18:23:21 -0300

    Added event script initial code

---
+++ chimera_eventscript/controllers/eventscript.py
@@ -0,0 +1,50 @@
+import ntpath
+import threading
+import os
+from chimera.interfaces.camera import CameraStatus
+from chimera.util.image import ImageUtil, Image
+from chimera.controllers.imageserver.util import getImageServer
+from chimera.core.chimeraobject import ChimeraObject
+
+import datetime as dt
+
+
+class EventScript(ChimeraObject):
+    __config__ = {
+        "camera": "/Camera/0",
+        "camera_readoutcomplete_script": None,
+    }
+
+    def _getCam(self):
+        return self.getManager().getProxy(self["camera"])
+
+    def __init__(self):
+        ChimeraObject.__init__(self)
+
+    def __start__(self):
+        self._getCam().readoutComplete += self.getProxy()._CameraReadoutCompleteClbk
+
+    def run_script(self, script, proxy, status):
+        if status == CameraStatus.OK:
+
+            image_fname = proxy.filename()
+            if ':\\' in image_fname:
+                modpath = ntpath
+            else:
+                modpath = os.path
+            image_fname = modpath.basename(image_fname)
+            image_type = proxy["IMAGETYP"] if "IMAGETYP"in proxy.keys() else "UNKNOWN"
+            print script, image_fname, dict(proxy)
+            os.system("%s %s %s" % (os.path.expanduser(script), image_fname, image_type))
+        return
+
+    def _CameraReadoutCompleteClbk(self, proxy, status):
+        self.log.debug("Acquired CameraReadoutComplete event")
+        if self["camera_readoutcomplete_script"] is not None:
+            p = threading.Thread(target=self.run_script, args=(self['camera_readoutcomplete_script'], proxy, status))
+            p.start()
+        else:
+            self.log.debug("Skipping camera_readoutcomplete_script is None")
+
+    def __stop__(self):
+        pass
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='chimera_template',
+    name='chimera_eventscript',
     version='0.0.1',
-    packages=['chimera_template', 'chimera_template.instruments', 'chimera_template.controllers'],
+    packages=['chimera_eventscript', 'chimera_eventscript.controllers'],
     scripts=[],
-    url='http://github.com/astroufsc/chimera-template',
+    url='http://github.com/astroufsc/chimera-eventscript',
     license='GPL v2',
     author='William Schoenell',
-    author_email='william@iaa.es',
-    description='Template for chimera plugins'
+    author_email='wschoenell@gmail.com  ',
+    description='Chimera template that runs scripts when events are triggered by an instrument.'
 )
